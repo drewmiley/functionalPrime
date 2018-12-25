4e21248a4acc93baec247c17789a0f3089b65e56
Drew Miley <drewmiley92@gmail.com>
2018-12-25 17:30:54 +0000

    Adds println for scala file

--- scalaFP/src/main/scala/Main.scala
+++ scalaFP/src/main/scala/Main.scala
@@ -1,3 +1,23 @@
 object Main extends App {
-    println("running")
+    println("Running Scala examples")
+
+
+    println("partial application")
+    println("a: A, f: (A, B) => C")
+    println("b: B { b => f(a, b) }")
+
+
+    println("currying")
+    println("f: (A, B) => C")
+    println("(a: A, b: B) { a => b => f(a, b) }")
+
+
+    println("uncurrying")
+    println("f: A => B => C")
+    println("(a: A, b: B) { (a, b) => f(a)(b) }")
+
+
+    println("composition")
+    println("f: A => B, g: B => C")
+    println("a: A { a => g(f(a)) }")
 }
