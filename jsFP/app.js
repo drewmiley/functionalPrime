93bc788d0addad6a8faf18d45843846aa130317b
Drew Miley <drewmiley92@gmail.com>
2018-12-25 17:09:13 +0000

    Adds console.log for js file

--- jsFP/app.js
+++ jsFP/app.js
@@ -1,1 +1,17 @@
-console.log('running');
+console.log('Running JavaScript examples');
+
+console.log('partial application');
+console.log('a: A, f: (A, B) => C');
+console.log('b: B { b => f(a, b) }');
+
+console.log('currying');
+console.log('f: (A, B) => C');
+console.log('(a: A, b: B) { a => b => f(a, b) }');
+
+console.log('uncurrying');
+console.log('f: A => B => C');
+console.log('(a: A, b: B) { (a, b) => f(a)(b) }');
+
+console.log('composition');
+console.log('f: A => B, g: B => C');
+console.log('a: A { a => g(f(a)) }');
