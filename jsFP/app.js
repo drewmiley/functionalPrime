b537d0d1b23ab837f5ba1ec1fde61966141270bb
Drew Miley <drewmiley92@gmail.com>
2018-12-25 17:19:30 +0000

    Adds js currying example

--- jsFP/app.js
+++ jsFP/app.js
@@ -17,6 +17,14 @@ console.log('currying');
 console.log('f: (A, B) => C');
 console.log('(a: A, b: B) { a => b => f(a, b) }');
 
+const currying = () => {
+    const f = (a, b) => (a * b).toString();
+    const currying = a => b => f(a, b);
+
+    console.log(currying(2.5)(2));
+}
+currying();
+
 console.log('uncurrying');
 console.log('f: A => B => C');
 console.log('(a: A, b: B) { (a, b) => f(a)(b) }');
