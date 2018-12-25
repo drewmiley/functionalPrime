34b49661941695ce203b2473bcd75208bb94b808
Drew Miley <drewmiley92@gmail.com>
2018-12-25 23:54:59 +0000

    Adds System.out.println for Java examples

--- javaFP/src/Main.java
+++ javaFP/src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("running");
+        System.out.println("Running Java examples");
+
+        System.out.println("partial application");
+        System.out.println("a: A, f: (A, B); => C");
+        System.out.println("b: B { b => f(a, b); }");
+
+        System.out.println("currying");
+        System.out.println("f: (A, B); => C");
+        System.out.println("(a: A, b: B); { a => b => f(a, b); }");
+
+        System.out.println("uncurrying");
+        System.out.println("f: A => B => C");
+        System.out.println("(a: A, b: B); { (a, b); => f(a);(b); }");
+
+        System.out.println("composition");
+        System.out.println("f: A => B, g: B => C");
+        System.out.println("a: A { a => g(f(a);); }");
     }
 }
