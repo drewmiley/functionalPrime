b024646cf2d1bb2f8ee46a6d640f48215494bb42
Drew Miley <drewmiley92@gmail.com>
2018-12-25 17:40:25 +0000

    Adds scala partial application example

--- scalaFP/src/main/scala/Main.scala
+++ scalaFP/src/main/scala/Main.scala
@@ -1,11 +1,17 @@
 object Main extends App {
     println("Running Scala examples")
 
-
     println("partial application")
     println("a: A, f: (A, B) => C")
     println("b: B { b => f(a, b) }")
 
+    private def partialApplication(): Unit = {
+        val a: Double = 2.5
+        def f(a: Double, b: Int): String = (a * b).toString
+        def partial(b: Int): String = f(a, b)
+        println(partial(2))
+    }
+    partialApplication()
 
     println("currying")
     println("f: (A, B) => C")
