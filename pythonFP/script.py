677cd8974cf138a874edb9837ab35a93c7d1fa8c
Drew Miley <drewmiley92@gmail.com>
2018-12-25 23:39:45 +0000

    Adds python composition example

--- pythonFP/script.py
+++ pythonFP/script.py
@@ -15,3 +15,14 @@ print('(a: A, b: B) { (a, b) => f(a)(b) }');
 print('composition');
 print('f: A => B, g: B => C');
 print('a: A { a => g(f(a)) }');
+
+def composition():
+    def f(a):
+        return str(2 * a)
+    def g(b):
+        return int(b[:1])
+    def composition(a):
+        return g(f(a))
+
+    print(composition(2.5))
+composition()
