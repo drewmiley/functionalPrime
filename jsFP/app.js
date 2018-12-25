04eb4859ec6f755c4881064c288f50c83919de81
Drew Miley <drewmiley92@gmail.com>
2018-12-25 17:17:17 +0000

    Adds js partial application exqmple

--- jsFP/app.js
+++ jsFP/app.js
@@ -4,6 +4,15 @@ console.log('partial application');
 console.log('a: A, f: (A, B) => C');
 console.log('b: B { b => f(a, b) }');
 
+const partialApplication = () => {
+    const a = 2.5;
+    const f = (a, b) => (a * b).toString();
+    const partial = b => f(a, b);
+
+    console.log(partial(2));
+}
+partialApplication();
+
 console.log('currying');
 console.log('f: (A, B) => C');
 console.log('(a: A, b: B) { a => b => f(a, b) }');
