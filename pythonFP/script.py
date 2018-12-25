8abb93203f0c051de1b744b394d7fce605a30f4b
Drew Miley <drewmiley92@gmail.com>
2018-12-25 19:45:29 +0000

    Adds print for python examples

--- pythonFP/script.py
+++ pythonFP/script.py
@@ -1,1 +1,17 @@
-print("running")
+print('Running JavaScript examples');
+
+print('partial application');
+print('a: A, f: (A, B) => C');
+print('b: B { b => f(a, b) }');
+
+print('currying');
+print('f: (A, B) => C');
+print('(a: A, b: B) { a => b => f(a, b) }');
+
+print('uncurrying');
+print('f: A => B => C');
+print('(a: A, b: B) { (a, b) => f(a)(b) }');
+
+print('composition');
+print('f: A => B, g: B => C');
+print('a: A { a => g(f(a)) }');
