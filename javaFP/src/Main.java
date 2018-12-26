35d2ed34b9de5cce38fdb99101dfbfa4378aa8b3
Drew Miley <drewmiley92@gmail.com>
2018-12-26 00:47:16 +0000

    Removes semi-colons

--- javaFP/src/Main.java
+++ javaFP/src/Main.java
@@ -7,24 +7,24 @@ public class Main {
         System.out.println("Running Java examples");
 
         System.out.println("partial application");
-        System.out.println("a: A, f: (A, B); => C");
-        System.out.println("b: B { b => f(a, b); }");
+        System.out.println("a: A, f: (A, B) => C");
+        System.out.println("b: B { b => f(a, b) }");
 
         partialApplication();
 
         System.out.println("currying");
-        System.out.println("f: (A, B); => C");
-        System.out.println("(a: A, b: B); { a => b => f(a, b); }");
+        System.out.println("f: (A, B) => C");
+        System.out.println("(a: A, b: B) { a => b => f(a, b) }");
 
         currying();
 
         System.out.println("uncurrying");
         System.out.println("f: A => B => C");
-        System.out.println("(a: A, b: B); { (a, b); => f(a);(b); }");
+        System.out.println("(a: A, b: B) { (a, b) => f(a)(b) }");
 
         System.out.println("composition");
         System.out.println("f: A => B, g: B => C");
-        System.out.println("a: A { a => g(f(a);); }");
+        System.out.println("a: A { a => g(f(a)) }");
     }
 
     static void partialApplication() {
